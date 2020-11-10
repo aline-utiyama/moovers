9fd6bdbdaa658aa73146f149346a96473c39a32a
aline utiyama <lineskutiyama@gmail.com>
2020-11-10 18:37:03 +0900

    Navbar and Booking page created

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-  
+
   def index
     @bookings = Booking.all
   end
@@ -8,28 +8,38 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
   end
 
+  def new
+    @mover = Mover.find(params[:mover_id])
+    @booking = Booking.new
+    authorize @booking
+  end
+
   def create
-    @book = Booking.new(booking_params)
-    @booking = Booking.find(params[:booking_id])
-    @booking.user = @user
-    if @booking.after_save
-      redirect_to users_path
+    @mover = Mover.find(params[:mover_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.mover = @mover
+    authorize @booking
+    if @booking.save
+      redirect_to bookings_path
     else
       render :new
     end
   end
-  
+
   def update
     @booking = Booking.find(params[:id])
 
     if @booking.update(mover.params)
       redirect_to @booking
     else
-      render 'edit'
+      render :edit
     end
   end
-    
-  private booking_params
+
+  private
+
+  def booking_params
     params.require(:booking).permit(:name, :type_of_car, :description, :price)
   end
 end
