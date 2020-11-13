3d4e1c9d3245334ca687d1eab5fd0ed91c6db28b
aline utiyama <lineskutiyama@gmail.com>
2020-11-13 12:03:17 +0900

    fixing migration

--- db/migrate/20201111133407_add_address_to_movers.rb
+++
@@ -1,5 +0,0 @@
-class AddAddressToMovers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :movers, :address, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_11_13_015505) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,9 +62,9 @@ ActiveRecord::Schema.define(version: 2020_11_13_015505) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
     t.float "latitude"
     t.float "longitude"
-    t.string "address"
     t.index ["user_id"], name: "index_movers_on_user_id"
   end
 
